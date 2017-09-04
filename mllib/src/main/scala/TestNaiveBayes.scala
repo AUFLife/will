96458a841e5fb77587c123511b2ce2aba60a326e
will <will@wuzhibo7696@163.com>
2017-09-04 22:30:31 +0800

    a test

---
+++ mllib/src/main/scala/TestNaiveBayes.scala
@@ -0,0 +1,42 @@
+import org.apache.log4j.lf5.util.Resource
+import org.apache.spark.mllib.classification.{NaiveBayes, NaiveBayesModel}
+import org.apache.spark.mllib.linalg.Vectors
+import org.apache.spark.mllib.regression.LabeledPoint
+import org.apache.spark.mllib.util.MLUtils
+import org.apache.spark.sql.SparkSession
+
+/**
+  * Created by wzb on 2017/9/4.
+  */
+object TestNaiveBayes extends App{
+
+  val spark = SparkSession
+    .builder()
+    .master("local[2]")
+    .appName("test native bayes")
+    .getOrCreate()
+  val sc = spark.sparkContext
+
+  val path = "D:\\GitHub\\will\\mllib\\src\\data\\mllib\\sample_naive_bayes_data.txt"
+  // Load and parse the data file.
+  val data =  sc.textFile(path)
+    .map{ line =>
+      val value = line.split(" ").toStream.map(_.toDouble).toArray
+      // LabeledPint代表一条训练数据，即打过标签的数据
+      new LabeledPoint(value(0), Vectors.dense(value))
+    }
+  val Array(training, test) = data.randomSplit(Array(0.6, 0.4))
+  val model = NaiveBayes.train(training, lambda = 1.0, modelType = "multinomial")
+
+  val predictionAndLabel = test.map(p => (model.predict(p.features), p.label))
+  val accuracy = 1.0 * predictionAndLabel.filter(x => x._1 == x._2).count() / test.count()
+
+  mode.save(sc, "")
+  val sameModel = NaiveBayesModel.load()
+
+
+
+
+
+
+}
